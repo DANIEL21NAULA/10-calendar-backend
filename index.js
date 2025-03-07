b21415bba50a2bccba00a3422ccbb898f4392b54
DANIEL21NAULA <163338712+DANIEL21NAULA@users.noreply.github.com>
2025-03-07 17:52:40 -0500

    Create User

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 require('dotenv').config();
+const { dbConnection } = require('./database/config');
 
 console.log(process.env.PORT)
 
 //* crear el servidor de express
 const app = express();
+
+//* Base de datos
+dbConnection();
 
 //* Directorio Public
 app.use(express.static('public'));
