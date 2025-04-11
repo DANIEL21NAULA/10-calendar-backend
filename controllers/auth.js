23dc53e9f02d9e0f0dd1c497f92e2a5b12b531e2
DANIEL21NAULA <163338712+DANIEL21NAULA@users.noreply.github.com>
2025-04-11 18:02:32 -0500

    crud auth y JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,36 +1,103 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const { generateJWT } = require('../helpers/jwt');
 
 const crearUsuario = async(req, res = response) => {
 
-    // const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-    const user = new User(req.body);
+    try {
+        let user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un Usuario existe con ese correo'
+            });
+        }
 
-    await user.save();
+        user = new User(req.body);
+        
+        //! Encripttar contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
 
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-    });
+        await user.save();
+
+        /*
+            * Generacion del token JWT
+        */
+
+        const token = await generateJWT(user.id, user.name);
+    
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor comuniquese con el administrador',
+        });
+    }
+
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
     const { email, password } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password,
-    });
+    try {
+        const  user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email'
+            });
+        }
+        //! Confirmar con los passwords
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if(!validPassword){
+            res.status(400).json({
+                ok: false,
+                msg: 'Contraseña incorrecta',
+            });
+        }
+
+        /*
+            * Generacion del token JWT
+        */
+        const token = await generateJWT(user.id, user.name);
+
+        res.status(200).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token,
+        });
+
+        
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor comuniquese con el administrador',
+        });
+    }
 };
 
-const revalidarToken = (req, res = response) => {
+const revalidarToken = async(req, res = response) => {
+    const { uid, name } = req
+
+    /*
+        * Generacion del token JWT
+    */
+    const token = await generateJWT(uid, name);
+
     res.json({
         ok: true,
-        msg: 'renew'
+        token,
     });
 };
 
